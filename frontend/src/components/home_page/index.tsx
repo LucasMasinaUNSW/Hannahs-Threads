0e76e11bf4aab77b3c788523f362c3e3f5abb9aa
LucasMasinaUNSW <z5204754@unsw.edu.au>
2021-07-06 12:36:47 +1000

    centered home page grid

--- frontend/src/components/home_page/index.tsx
+++ frontend/src/components/home_page/index.tsx
@@ -1,29 +1,35 @@
 import Activity from "./activity_element";
 import Banner from "./banner";
 import Grid from '@material-ui/core/Grid';
+import CSS from 'csstype';
+
+const inlineStyles: CSS.Properties = {
+  margin: 'auto',
+  width: '50%',
+}
 
 function Home() {
   return (
     <div className="Home page">
       <Banner/>
-      <Grid container spacing={2} justify="center" alignItems="center">
-        <Grid item xs>
-          <Grid container direction="column" spacing={3}>
-            <Grid item xs>
+      <Grid container spacing={0} justify="center" alignItems="center" style={inlineStyles}>
+        <Grid container item xs={6}>
+          <Grid container item direction="column" spacing={10}>
+            <Grid item xs={12}>
               <Activity activityName="designs"/>
             </Grid>
-            <Grid item xs>
-            <Activity activityName="fabrics"/>
+            <Grid item xs={12}>
+              <Activity activityName="fabrics"/>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs>
-          <Grid container direction="column" spacing={1}>
-            <Grid item xs>
+        <Grid container item xs={6}>
+          <Grid container item direction="column" spacing={1}>
+            <Grid item xs={12}>
               <Activity activityName="closet"/>
-            </Grid>  
+            </Grid>
           </Grid>
-          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
