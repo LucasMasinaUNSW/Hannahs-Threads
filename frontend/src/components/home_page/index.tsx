7e5fc6ca6a33b9ba11874c41324c181d27afd1bb
LucasMasinaUNSW <z5204754@unsw.edu.au>
2021-07-06 12:00:02 +1000

    initial grid layout

--- frontend/src/components/home_page/index.tsx
+++ frontend/src/components/home_page/index.tsx
@@ -1,13 +1,30 @@
 import Activity from "./activity_element";
 import Banner from "./banner";
+import Grid from '@material-ui/core/Grid';
 
 function Home() {
   return (
     <div className="Home page">
       <Banner/>
-      <Activity activityName="closet"/>
-      <Activity activityName="designs"/>
-      <Activity activityName="fabrics"/>
+      <Grid container spacing={2} justify="center" alignItems="center">
+        <Grid item xs>
+          <Grid container direction="column" spacing={3}>
+            <Grid item xs>
+              <Activity activityName="designs"/>
+            </Grid>
+            <Grid item xs>
+            <Activity activityName="fabrics"/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs>
+          <Grid container direction="column" spacing={1}>
+            <Grid item xs>
+              <Activity activityName="closet"/>
+            </Grid>  
+          </Grid>
+          </Grid>
+      </Grid>
     </div>
   );
 }
