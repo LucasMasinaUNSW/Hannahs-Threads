4e66695dfabcc3e7fa5d9eea7cbd4a544213bc63
LucasMasinaUNSW <z5204754@unsw.edu.au>
2021-07-07 17:57:06 +1000

    more styling

--- frontend/src/components/home_page/banner/index.tsx
+++ frontend/src/components/home_page/banner/index.tsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  root: {
+    color: '#000000',
+  },
+});
+
 function Banner() {
-    return (
-      <div className="Banner">
-        <h1>Hannah's Threads</h1>
-      </div>
-    );
-  }
+
+  let classes = useStyles();
+
+  return (
+    <Typography className={classes.root} variant="h1" component="h2">Hannah's Threads</Typography>
+  );
+}
   
 export default Banner;
